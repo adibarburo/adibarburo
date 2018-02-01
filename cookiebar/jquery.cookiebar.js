293d030a7ddcd21f4ed4bdfabb9b3d4a1bf89c67
Adi Barbu <adibarburo@users.noreply.github.com>
2018-02-01 14:13:44 +0200

    Update jquery.cookiebar.js

--- cookiebar/jquery.cookiebar.js
+++ cookiebar/jquery.cookiebar.js
@@ -32,7 +32,7 @@
 			declineFunction: function(cookieValue){if(cookieValue=='enabled' || cookieValue=='accepted') window.location = window.location.href;}, //Function to run after decline
 			policyButton: true, //Set to true to show Privacy Policy button
 			policyText: 'Politica cookies', //Text on Privacy Policy button
-			policyURL: '/privacy-policy/', //URL of Privacy Policy
+			policyURL: '/p/utilizare-cookie-uri.html', //URL of Privacy Policy
 			autoEnable: true, //Set to true for cookies to be accepted automatically. Banner still shows
 			acceptOnContinue: false, //Set to true to accept cookies when visitor moves to another page
 			acceptOnScroll: false, //Set to true to accept cookies when visitor scrolls X pixels up or down
@@ -43,8 +43,8 @@
 			effect: 'slide', //Options: slide, fade, hide
 			element: 'body', //Element to append/prepend cookieBar to. Remember "." for class or "#" for id.
 			append: false, //Set to true for cookieBar HTML to be placed at base of website. Actual position may change according to CSS
-			fixed: false, //Set to true to add the class "fixed" to the cookie bar. Default CSS should fix the position
-			bottom: false, //Force CSS when fixed, so bar appears at bottom of website
+			fixed: true, //Set to true to add the class "fixed" to the cookie bar. Default CSS should fix the position
+			bottom: true, //Force CSS when fixed, so bar appears at bottom of website
 			zindex: '', //Can be set in CSS, although some may prefer to set here
 			domain: String(window.location.hostname), //Location of privacy policy
 			referrer: String(document.referrer) //Where visitor has come from
